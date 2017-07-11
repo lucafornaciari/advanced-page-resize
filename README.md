b59373c2d6c915ce0a34ef916620f935267a75d2
luca.fornaciari <luca.fornaciari@l-fornaciari.client.ns.imm.re>
2017-07-11 14:51:03 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # advancedOnResize
 advancedOnResize provide a simple solution to check window-x-resize, window-y-resize
+
+## Installation
+
+  `npm install advanced-page-resize`
+
+## Usage
+
+    var pageResize = require('advanced-page-resize');
+
+    pageResize.watch({
+        action: function(){
+            console.log('execute this action on resizing page')
+        },
+        limit: 'X',
+    });
---
+++ index.js
@@ -0,0 +1,73 @@
+import throttle from 'raf-throttle'
+
+const defaultOpts = {
+    action: () => { },
+    limit: null,
+    throttle: false
+}
+const LIMITX = 'X';
+const LIMITY = 'Y';
+
+class advancedPageResize {
+    constructor(opts) {
+        const options = {
+            ...defaultOpts,
+            ...opts
+        }
+
+        this.options = options;
+        this.lastWidth = window.innerWidth;
+        this.lastHeight = window.innerHeight;
+
+        let callback = this._getCallback();
+        resize(callback);
+    }
+
+    resize(callback) {
+        window.addEventListener('resize', callback);
+    }
+
+    _getCallback() {
+        if (this.options.limit === null || (this.options.limit !== LIMITX && this.options.limit !== LIMITY)) {
+            return (this.options.throttle) ? throttle(this.options.action) : this.options.action;
+        } else {
+            let callback = (this.options.limit === LIMITX) ? this._resizeX() : this._resizeY();
+            return (this.options.throttle) ? throttle(callback) : callback;
+        }
+    }
+
+    _resizeY() {
+        return () => {
+            if (window.innerHeight !== this.lastHeight) {
+                this.options.action();
+            }
+            this._setDimensions();
+        };
+    }
+
+    _resizeX() {
+        return () => {
+            if (window.innerWidth !== this.lastWidth) {
+                this.options.action();
+            }
+            this._setDimensions();
+        };
+    }
+
+    _setDimensions() {
+        this.lastWidth = window.innerWidth;
+        this.lastHeight = window.height;
+    }
+}
+
+module.exports = (opts) => {
+    function watch(opts = {}) {
+        new advancedPageResize(opts);
+    }
+
+    let root = {
+        watch: watch
+    }
+
+    return root;
+}
